bfe9e88139ef7dd5ce2bdd7e678c5943666d3728
ariksonek <ariksonek@gmail.com>
2021-11-08 14:28:27 +0100

    Add files via upload

---
+++ RB/cookiescript.js
@@ -0,0 +1,8 @@
+
+Cookies.set('kolkidrewniane', 'true',  { expires: 5 }); 
+Cookies.set('kolkidrewniane', 'true', { secure: true });
+Cookies.set('kolkidrewniane', 'true', { sameSite: 'strict'});
+
+
+
+var myCookie = Cookies.get('kolkidrewniane');
